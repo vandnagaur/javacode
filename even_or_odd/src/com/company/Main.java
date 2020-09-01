a0557a366de61f62252ca9aa0d915bb6b7936aee
vandnagaur <70502888+vandnagaur@users.noreply.github.com>
2020-09-01 17:17:29 +0530

    Add files via upload

---
+++ even_or_odd/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 11;
+
+	if (a % 2 == 0){
+
+        System.out.println("number is even");
+    }
+	else{
+        System.out.println("number is odd");
+    }
+    }
+}
