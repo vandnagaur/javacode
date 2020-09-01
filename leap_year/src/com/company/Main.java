7bef3506f589b8256ba460638cdbeb2a031ed282
vandnagaur <70502888+vandnagaur@users.noreply.github.com>
2020-09-01 17:43:39 +0530

    Add files via upload

---
+++ leap_year/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 3000;
+
+	if (a % 400 == 0){
+
+        System.out.println("this is a leap year");
+    }
+	else {
+
+        System.out.println("this isn't a leap year");
+    }
+    }
+}
