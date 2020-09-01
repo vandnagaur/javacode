0ed691f9918dbb9fff5a7afe9f186a85f48a0167
vandnagaur <70502888+vandnagaur@users.noreply.github.com>
2020-09-01 18:06:49 +0530

    Add files via upload

---
+++ swapping/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 10;
+	int b = 20;
+
+        System.out.println("a =" +a);
+        System.out.println("b =" +b);
+
+        System.out.println("swapped value of a =" +b);
+        System.out.println("swapped value of b =" +a);
+    }
+}
